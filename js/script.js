e548a61207cbfc5c6d1f16daa15e658f83b8388e
Adam Kurczynski <adkurczynskis@interia.pl>
2019-09-11 13:36:22 +0200

    Finish website

--- js/script.js
+++ js/script.js
@@ -28,12 +28,46 @@ function titleClickHandler(event){
  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
- /* add class 'active' to the correct article */
+ /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /*[DONE]  remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+
+  function clearMessages(){
+    titleList.innerHTML = '';
+  }
+  clearMessages();
+  let html = '';
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  for(let article of articles){
+    /* [DONE] get the article id */
+    const articleId = article.getAttribute('id');
+    /* [DONE] find the title element */
+    /* [DONE] get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* [DONE] create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    /* [DONE] insert link into titleList */
+    html = html + linkHTML;
+    console.log(html)
+  }
+    titleList.insertAdjacentHTML('afterbegin', html);
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
 }
+
+generateTitleLinks();
